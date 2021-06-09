f622b0f7afed0723243e6eae96cfdc9c0b6e1461
Roman Malyk <rmalykzp@gmail.com>
2021-06-09 12:40:51 +0300

    doble-slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,35 @@ $(function () {
     $('.slider-box').slick({
         prevArrow: '<button class="slider-btn-left"></button>',
         nextArrow: '<button class="slider-btn-right"></button>',
-        dots: true, 
+        dots: true,
         dotsClass: 'slick-dots',
     });
+});
+
+let stepsSlider = document.getElementById('slider-floor');
+let input0 = document.getElementById('input-with-keypress-0');
+let input1 = document.getElementById('input-with-keypress-1');
+let inputs = [input0, input1];
+
+
+
+noUiSlider.create(stepsSlider, {
+    start: [6, 21],
+    connect: true,
+    step: 1,
+    range: {
+        'min': 6,
+        'max': 21
+    },
+    format: {
+        to: function (value) {
+            return parseInt(value);
+        },
+        from: function (value) {
+            return parseInt(value);
+        }
+    }
+});
+stepsSlider.noUiSlider.on('update', function (values, handle) {
+    inputs[handle].value = values[handle];
 });
--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,8 @@ function scripts() {
 function js() {
     return src([
         'node_modules/jquery/dist/jquery.js',
-        'node_modules/slick-carousel/slick/slick.js'
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/nouislider/dist/nouislider.min.js'
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
@@ -41,6 +42,7 @@ function styles() {
     return src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
+        'node_modules/nouislider/dist/nouislider.min.css',
         'app/scss/style.scss'
     ])
         .pipe(concat('style.min.css'))
